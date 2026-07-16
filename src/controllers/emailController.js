52cc98796e8fe796c4a77f9f74a7a646ef616dfa
donald-hub <56503357+donald-hub@users.noreply.github.com>
2026-07-16 19:35:11 +0530

    correction

--- src/controllers/emailController.js
+++ src/controllers/emailController.js
@@ -1,6 +1,8 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+const dotenv = dotenv.config();
 
 export const forgotPassword = async (req, res) => {
   try {
